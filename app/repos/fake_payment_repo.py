48cf8f02c88b781edd7f76bc1cc9e131686a4e9b
Keith James Seruyange <keithseruyange@gmail.com>
2026-03-12 02:24:12 +0300

    Added the customer functions in the FakePaymentRepo

--- app/repos/fake_payment_repo.py
+++ app/repos/fake_payment_repo.py
@@ -3,3 +3,16 @@ class FakePaymentRepo:
         self.customers = {}
         self.payments = {}
         self.refunds = {}
+
+    def save_customer(self, customer):
+        self.customer[customer["id"]] = customer
+        return customer
+    
+    def find_customer_by_id(self, customer_id):
+        return self.customers.get(customer_id)
+    
+    def find_customer_by_email(self, email):
+        for customer in self.customers.values():
+            if customer['email'] == email:
+                return customer
+        return None
