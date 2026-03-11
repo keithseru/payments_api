342e7429fd4420634d756c47a95ba11e407f1336
Keith James Seruyange <keithseruyange@gmail.com>
2026-03-12 00:57:58 +0300

    created the inital PaymentService class and imported the validators

--- services/payment_service.py
+++ services/payment_service.py
@@ -1,8 +1,8 @@
 from app.utils.validators import (
-    validateAmount,
-    validateCurrency,
-    validateEmail,
-    generateId,
+    validate_amount,
+    validate_currency,
+    validate_email,
+    generate_id,
 )
 class PaymentService:
     pass
