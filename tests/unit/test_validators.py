a8502509b0cb4ddbe3a8f6034e7921aaa2d9dd0f
Keith James Seruyange <keithseruyange@gmail.com>
2026-03-11 22:59:10 +0300

    Created a pytest.ini file that add the current directory to the PYTHONPATH

--- tests/unit/test_validators.py
+++ tests/unit/test_validators.py
@@ -1,12 +1,12 @@
 import unittest
-from app.utils import validateAmount
+from app.utils import validators
 
 class ValidateAmount(unittest.TestCase):
     def test_returns_true_amount_positive():
         '''
         Returns true if amount is a positive integer
         '''
-        assert validateAmount(100) is True
+        assert validators.validateAmount(100) is True
 
 if __name__ == "__main__":
     unittest.main()
