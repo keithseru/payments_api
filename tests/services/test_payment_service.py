cce441b95e84994193acc35009f2382d5c9844f0
Keith James Seruyange <keithseruyange@gmail.com>
2026-03-12 01:31:23 +0300

    Created the test to check that customer returns customer woth correct name and email tha failed

--- tests/services/test_payment_service.py
+++ tests/services/test_payment_service.py
@@ -3,7 +3,12 @@ from app.services.payment_service import PaymentService
 from app.repos.fake_payment_repo import FakePaymentRepo
 
 class PaymentServiceTest(unittest.TestCase):
+    def setUp(self):
+        self.repo = FakePaymentRepo()
+        self.service = PaymentService(self.repo)
+    
     def test_create_customer_returns_customer_with_correct_name_and_email(self):
-        customer = self.create_customer("Alice", "alice@example.com")
+        customer = self.service.create_customer("Alice", "alice@example.com")
         
         self.assertEqual(customer['name'], "Alice")
+        self.assertEqual(customer['email'], 'alice@example.com')
