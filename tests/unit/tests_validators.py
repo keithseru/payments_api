6f55dd6c6707422cef8b8e8d2df890190a20e2c9
Keith James Seruyange <keithseruyange@gmail.com>
2026-03-11 22:42:08 +0300

    Created test for test returns true if amount is a positive integer

---
+++ tests/unit/tests_validators.py
@@ -0,0 +1,10 @@
+import unittest
+from app.utils import validateAmount
+
+class ValidateAmount(unittest.TestCase):
+    def test_returns_true_amount_positive():
+        '''
+        Returns true if amount is a positive integer
+        '''
+        assert validateAmount(100) is True
+        
